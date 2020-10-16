fd385860648c81e5915a8b9b8a356ede1ed8292d
Mihai Chifor <chifor.mihai21@gmail.com>
2020-10-16 13:34:56 +0300

    Add config checks and some tests for those checks

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,167 @@
-it.todo('write a test');
+// @ts-nocheck
+
+import BerbixSdk, { BerbixConfiguration, BerbixEnvironment } from '../index';
+
+// add mock
+jest.mock(
+  '../../node_modules/react-native/Libraries/BatchedBridge/NativeModules',
+  () => {
+    return {
+      BerbixSdk: {
+        startFlow: jest.fn().mockReturnValue(Promise.resolve()),
+        createSession: jest.fn().mockReturnValue(Promise.resolve()),
+        displayFlow: jest.fn().mockReturnValue(Promise.resolve()),
+      },
+    };
+  }
+);
+
+const RESOLVED = 'resolved';
+const REJECTED = 'rejected';
+
+const startFlow = (config: BerbixConfiguration) => {
+  return BerbixSdk.startFlow(config)
+    .then(() => {
+      return RESOLVED;
+    })
+    .catch(() => {
+      return REJECTED;
+    });
+};
+
+const createSession = (config: BerbixConfiguration) => {
+  return BerbixSdk.createSession(config)
+    .then(() => {
+      return RESOLVED;
+    })
+    .catch(() => {
+      return REJECTED;
+    });
+};
+
+const validConfig = {
+  clientToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjU3MjEwODc4OTc3NjM4NDAsImNpZCI6NTIyODYzOTMyOTE4OTg4OCwiY3R0IjoxLCJhdWQiOiJ2ZXJpZnkuYmVyYml4LmNvbSIsImV4cCI6MTYwMjY4MjgyMiwiaWF0IjoxNjAyNjc5MjIyLCJzdWIiOiJjYyJ9.8ZYbTkQvnjj6uk0e9YCsBeErRNB4bAM9wI0XEA6-d28',
+};
+
+/* Start flow tests */
+
+it('should reject when starting the flow without a config', () => {
+  return startFlow(null).then((result) => expect(result).toBe(REJECTED));
+});
+
+it('should reject when starting the flow without setting up the clientToken in config', () => {
+  return startFlow({ environment: BerbixEnvironment.staging }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when starting the flow with an invalid JWT *clientToken*', () => {
+  return startFlow({ clientToken: 'invalid_jwt' }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should resolve when starting the flow with a config that contains only a valid JWT *clientToken*', () => {
+  return startFlow({
+    clientToken: validConfig.clientToken,
+  }).then((result) => expect(result).toBe(RESOLVED));
+});
+
+it('should reject when starting the flow with an invalid *environment*', () => {
+  return startFlow({
+    ...validConfig,
+    environment: 'invalid env',
+  }).then((result) => expect(result).toBe(REJECTED));
+});
+
+it('should reject when starting the flow with a config containing *baseUrl* that is not of type string', () => {
+  return startFlow({ baseUrl: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when starting the flow with a config containing *debug* that is not of type boolean', () => {
+  return startFlow({ debug: 'true' }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when starting the flow with a config containing *email* that is not of type string', () => {
+  return startFlow({ email: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when starting the flow with a config containing *phone* that is not of type string', () => {
+  return startFlow({ phone: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when starting the flow with a config containing *templateKey* that is not of type string', () => {
+  return startFlow({ templateKey: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+/* Create session tests */
+
+it('should reject when creating a session without a config', () => {
+  return createSession(null).then((result) => expect(result).toBe(REJECTED));
+});
+
+it('should reject when creating a session without setting up the *clientToken* in config', () => {
+  return createSession({
+    environment: BerbixEnvironment.staging,
+  }).then((result) => expect(result).toBe(REJECTED));
+});
+
+it('should reject when creating a session with an invalid JWT *clientToken*', () => {
+  return createSession({ clientToken: 'invalid_jwt' }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when creating a session with an invalid *environment*', () => {
+  return createSession({
+    ...validConfig,
+    environment: 'invalid env',
+  }).then((result) => expect(result).toBe(REJECTED));
+});
+
+it('should reject when creating a session with a config containing *baseUrl* that is not of type string', () => {
+  return createSession({ baseUrl: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when creating a session with a config containing *debug* that is not of type boolean', () => {
+  return createSession({ debug: 'true' }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when creating a session with a config containing *email* that is not of type string', () => {
+  return createSession({ email: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when creating a session with a config containing *phone* that is not of type string', () => {
+  return createSession({ phone: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should reject when creating a session with a config containing *templateKey* that is not of type string', () => {
+  return createSession({ templateKey: true }).then((result) =>
+    expect(result).toBe(REJECTED)
+  );
+});
+
+it('should resolve when creating a session with a config that contains only a valid JWT clientToken', () => {
+  return createSession({
+    clientToken: validConfig.clientToken,
+  }).then((result) => expect(result).toBe(RESOLVED));
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { NativeModules } from 'react-native';
 
+const { BerbixSdk } = NativeModules;
+
 export enum BerbixEnvironment {
   development,
   sandbox,
@@ -7,10 +9,16 @@ export enum BerbixEnvironment {
   production,
 }
 
-type BerbixConfiguration = {
+export type BerbixConfiguration = {
   clientToken: string;
   baseUrl?: string;
   environment?: BerbixEnvironment;
+
+  /* Android only */
+  debug?: boolean;
+  email?: string;
+  phone?: string;
+  templateKey?: string;
 };
 
 type BerbixSdkType = {
@@ -19,6 +27,74 @@ type BerbixSdkType = {
   displayFlow(): Promise<void>;
 };
 
-const { BerbixSdk } = NativeModules;
+const checkConfigValidity = (config: BerbixConfiguration): Error | null => {
+  if (!config) {
+    return new Error('config not provided');
+  }
 
-export default BerbixSdk as BerbixSdkType;
+  if (!config.clientToken) {
+    return new Error('clientToken not provided');
+  }
+
+  if (
+    !config.clientToken.match(
+      /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/
+    )
+  ) {
+    return new Error('clientToken is not a valid JWT token');
+  }
+
+  if (config.environment && !(config.environment in BerbixEnvironment)) {
+    return new Error('environment is not of BerbixEnvironment type');
+  }
+
+  if (config.baseUrl && typeof config.baseUrl !== 'string') {
+    return new Error('baseUrl must be string');
+  }
+
+  if (config.debug && typeof config.debug !== 'boolean') {
+    return new Error('debug must be boolean');
+  }
+
+  if (config.email && typeof config.email !== 'string') {
+    return new Error('email must be string');
+  }
+
+  if (config.phone && typeof config.phone !== 'string') {
+    return new Error('phone must be string');
+  }
+
+  if (config.templateKey && typeof config.templateKey !== 'string') {
+    return new Error('templateKey must be string');
+  }
+
+  return null;
+};
+
+const Berbix: BerbixSdkType = {
+  startFlow(config: BerbixConfiguration): Promise<void> {
+    const configError = checkConfigValidity(config);
+
+    if (configError) {
+      return Promise.reject(configError);
+    }
+
+    return BerbixSdk.startFlow(config);
+  },
+
+  createSession(config: BerbixConfiguration): Promise<void> {
+    const configError = checkConfigValidity(config);
+
+    if (configError) {
+      return Promise.reject(configError);
+    }
+
+    return BerbixSdk.createSession(config);
+  },
+
+  displayFlow(): Promise<void> {
+    return BerbixSdk.displayFlow();
+  },
+};
+
+export default Berbix;
